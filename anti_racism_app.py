fad5b772a0939e6416bb4e6125a3c3f9a15efdfa
Devang Vyas <builderwebsite_94@gmail.com>
2026-04-09 10:33:11 +0530

    docs: added comprehensive logic comments Sentinel-NLP program

--- anti_racism_app.py
+++ anti_racism_app.py
@@ -2,7 +2,7 @@ print("Welcome to the Anti-Racist Project.")
 print("This Is a Platform to Make the World a Better Place for Everyone.")
 # Function to Check the Racism
 def check(racist_text):
-    # The 
+    # The List of Dictionary Where the Key Indicates the Type and Values are the words to flag.
     toxic_words = {
         "toxic": ["jerk", "pervert", "pathetic", "don't deserve", "hate"],
         "racist": ["nigga", "gypped", "uppity", "peanut gallery", "black", "dark"],
@@ -11,6 +11,7 @@ def check(racist_text):
     }
     found = False
     text_lower = racist_text.lower()
+    # logic of finding and printing the type of words with reccomendation
     for key, value in toxic_words.items():
         for word in value:
             words_in_input = text_lower.split() 
@@ -25,9 +26,10 @@ def check(racist_text):
                     print("The statement was vulgar.")
                 else:
                     print("Body shaming is wrong and harmful.")
+    # if no racist or Inappropriate word is found then
     if not found:
         print("No toxic or racist language was detected.")
-# The variable
+# The variable that inputs the sentence
 racist_text = input("Enter the message to validate: ")
 print("The engine is starting to analyse.")
 check(racist_text)
